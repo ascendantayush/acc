b4afecb84bec1a71da375e6de2e7fe1cfc996116
Ayush Raj <161280074+ascendantayush@users.noreply.github.com>
2025-08-21 09:31:10 +0530

    Update seieve.java

---
+++ seieve.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class seieve{
+    
+    static void seive(int n ){
+        int[] prime = new int[n+1];
+
+        // 0 = not prime and 1 = prime
+        
+        prime[0] = 0;
+        prime[1] = 0;
+
+        for(int i = 2; i < prime.length ; i++){
+            prime[i] = 1;
+        }
+
+        for(int p = 2 ; p * p < prime.length ; p++){
+            if(prime[p] == 1){
+                for(int i = p * p ; i <= n ; i =i + p){
+                    prime[i]=0;
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i= 0 ; i < prime.length ; i++){
+            if(prime[i] == 1){
+                count++;
+                System.out.printf("%d ",i);
+            }
+
+        }
+
+
+        System.out.println();
+
+
+        System.out.println(count);
+
+        
+
+    }
+
+
+    public static void main(String args[]){
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        seive(n);
+    }
+}
