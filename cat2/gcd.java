31b6abb1ee4ca90cf4acaef36b57d4220057c532
Ayush Raj <161280074+ascendantayush@users.noreply.github.com>
2025-10-09 06:25:46 +0530

    Create gcd.java

---
+++ cat2/gcd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class gcd {
+    public static void main(String[] agrs){
+        Scanner cin = new Scanner(System.in);
+
+        int a = cin.nextInt();
+        int b = cin.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        while(b!=0){
+            int temp = b;
+            b = b % a;
+            a = temp;
+        }
+
+        System.out.println(a);
+    }
+}
