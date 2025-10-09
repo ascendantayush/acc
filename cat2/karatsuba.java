6b4015bb8807bb466a0aa736e4cfe9f365033092
Ayush Raj <161280074+ascendantayush@users.noreply.github.com>
2025-10-09 06:26:11 +0530

    Implement basic Karatsuba multiplication in Java

---
+++ cat2/karatsuba.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class karatsuba {
+   public static void main(String[] args){
+    Scanner cin = new Scanner(System.in);
+
+    long a = cin.nextLong();
+    long b = cin.nextLong();
+
+    System.out.println(a * b);
+    cin.close();
+   } 
+}
