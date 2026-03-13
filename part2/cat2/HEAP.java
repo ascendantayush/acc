bf1efe83917f4f7f7456a854841006c5af02585f
Ayush Raj <161280074+ascendantayush@users.noreply.github.com>
2026-03-13 09:55:08 +0530

    Add heap sort implementation in HEAP.java
    Implement heap sort algorithm with heapify and build_heap methods.

---
+++ part2/cat2/HEAP.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class test {
+
+    static void printer(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println("");
+    }
+
+    static void heapify(int[] arr,int n,int i){
+        int largest = i ;
+        int left  = 2*i + 1;
+        int right = 2*i + 2;
+
+        if(left < n && arr[left] > arr[largest]){
+            largest = left;
+        }
+
+        if(right < n && arr[right] > arr[largest]){
+            largest = right;
+        }
+
+        if(largest != i){
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
+        }
+    }
+    
+    static void build_heap(int[] arr){
+        int n = arr.length;
+
+        for(int i = n/2 - 1; i >=0 ; i--){
+            heapify(arr, n, i);
+        }
+        
+        for(int  i = n -1  ; i >0 ; i--){
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i,0);
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = cin.nextInt();
+        }
+        build_heap(arr);
+
+        printer(arr);
+
+
+    }
+}
+
+
+
+
+
+
+// trick use prebuilt in function of priority queue 
+
+
+// just sort  the array
